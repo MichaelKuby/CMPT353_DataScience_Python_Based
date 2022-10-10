c50a580820a4320d3cc5bc2d05fc8dfa7c313d79
MichaelKuby <105996614+MichaelKuby@users.noreply.github.com>
2022-10-10 12:02:49 -0700

    Exercise 5

--- Exercise 5/reddit_weekends.py
+++ Exercise 5/reddit_weekends.py
@@ -1,4 +1,8 @@
 import sys
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
 
 
 OUTPUT_TEMPLATE = (
@@ -16,8 +20,22 @@ OUTPUT_TEMPLATE = (
 
 def main():
     reddit_counts = sys.argv[1]
-
-    # ...
+    counts = pd.read_json(reddit_counts, lines = True)
+    
+    # filter by dates including years 2012 and 2013 only, and by the subreddit Canada
+    df_2012_13 = counts[(counts['date'].dt.year >= 2012) & (counts['date'].dt.year < 2014) & (counts['subreddit'] == 'canada')]
+    df_2012_13_wkday = df_2012_13[df_2012_13['date'].dt.dayofweek < 5]
+    df_2012_13_wkend = df_2012_13[df_2012_13['date'].dt.dayofweek >4]
+    
+    # Use scipy.stats to do a T-test on the data to get a p-value.
+    # Can we conclude that there are a different number of comments on weekdays
+    # compared to weekends?
+    
+    plt.hist(df_2012_13_wkday['comment_count'])
+    plt.hist(df_2012_13_wkend['comment_count'])
+    plt.show()
+    ttest = stats.ttest_ind(df_2012_13_wkday['comment_count'], df_2012_13_wkend['comment_count'])
+    print(ttest)
 
     print(OUTPUT_TEMPLATE.format(
         initial_ttest_p=0,
